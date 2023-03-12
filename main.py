27c826d3605fa8d086b3c01c65e46ecf2c8b7083
nathanieluriri <95766173+nathanieluriri@users.noreply.github.com>
2023-03-12 16:40:18 +0100

    renamed:    app.py -> main.py new file:   templates/aboutus.html

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app=Flask(__name__)
 @app.route("/home")
 def home():
     return render_template("index.html")
-
+@app.route("/about")
+def about():
+    return render_template('aboutus.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
