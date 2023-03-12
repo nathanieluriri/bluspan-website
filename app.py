7d48c8256fe77bdb6f84fece46b780f4e55c5c6a
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2023-03-12 21:15:38 +0100

    created industries-we-serve page

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app=Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/services")
+@app.route("/industries-we-serve")
+def industriesWeServe():
+    return render_template("industries-we-serve.html")
+    
 if __name__ == '__main__':
     app.run(debug=True)
