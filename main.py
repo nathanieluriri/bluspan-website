baf22102ac10281be0769ad9719951d8527dd367
nathanieluriri <95766173+nathanieluriri@users.noreply.github.com>
2023-03-15 18:30:31 +0100

    new file:   templates/values.html modified:   main.py
    Added route for the values page and about to write more of the html
    and css

--- main.py
+++ main.py
@@ -15,5 +15,9 @@ def industriesWeServe():
 @app.route("/about")
 def about():
     return render_template('aboutus.html')
+
+@app.route("/values")
+def values():
+    return render_template('values.html')
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
